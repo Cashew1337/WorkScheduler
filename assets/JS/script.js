b4ad5f35afa81d3e4f9a11f4b0293a7ac7f0286d
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-02-28 15:11:35 -0500

    Commit to push current code to repo before reaching out to AskBCS.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,9 +7,6 @@ $(function dayPlanner() {
   var save = $('.saveBtn');
   var hourIndex = $('.dailyPlan')
 
-  console.log(hourIndex)
-
-
   //Creates local storage array for saved events
   let eventStorage = JSON.parse(localStorage.getItem('event'));
 
@@ -18,39 +15,37 @@ $(function dayPlanner() {
   } else {
     eventTextArry = new Array(9)
   };
-
-
+  console.log(eventTextArry)
+  //Function to add IDs to all textareas
   function addId() {
     for (let i = 0; i < hourIndex.length; i++) {
       hourIndex[i].id = 'hour-' + (i)
-      var hourId = hourIndex[i].id.split('hour-').join('')
-      var hourNum = parseInt(hourId);
       // console.log(hourIndex, currentTime.$H - 9)
-      console.log(hourIndex[i].id, hourId)
-      console.log(hourNum < currentTime.$H - 9, hourNum === currentTime.$H - 9, hourNum > currentTime.$H - 9)
-    } 
+      // console.log(hourIndex[i].id, hourId)
+      // console.log(hourNum < currentTime.$H - 9, hourNum === currentTime.$H - 9, hourNum > currentTime.$H - 9)
+    }
   }
 
-  function addRemoveClass () {
+  //Function to add/remove classes based on ID number compared to current hour
+  function addRemoveClass() {
     for (let i = 0; i < hourIndex.length; i++) {
       var hourId = hourIndex[i].id.split('hour-').join('')
       var hourNum = parseInt(hourId);
-      var hourPlan = hourIndex[i]
-      console.log(hourPlan.id)
-    if (hourNum < currentTime.$H - 9) {
-      console.log("if")
-      hourIndex[i].classList.remove('future');
-      hourIndex[i].classList.add('past');
-    } else if (hourNum === currentTime.$H - 9) {
-      console.log("else if 2")
-      hourIndex[i].classList.remove('past');
-      hourIndex[i].classList.add('present');
-    } else {
-      console.log("else if")
-      hourIndex[i].classList.remove('present');
-      hourIndex[i].classList.add('future');
+      // console.log(hourPlan.id)
+      if (hourNum < currentTime.$H - 9) {
+        // console.log("if")
+        hourIndex[i].classList.remove('future');
+        hourIndex[i].classList.add('past');
+      } else if (hourNum === currentTime.$H - 9) {
+        // console.log("else if 2")
+        hourIndex[i].classList.remove('past');
+        hourIndex[i].classList.add('present');
+      } else {
+        // console.log("else if")
+        hourIndex[i].classList.remove('present');
+        hourIndex[i].classList.add('future');
+      }
     }
-  }
   }
 
   addId();
@@ -58,14 +53,30 @@ $(function dayPlanner() {
   // console.log(hourIndex)
   // console.log(currentTime.$H - 9)
   // console.log(eventTextArry)
+  function addSaveId() {
+    for (let i = 0; i < hourIndex.length; i++) {
+      save[i].id = 'saveId-' + (i)
+    }
+  }
+  addSaveId();
+  console.log(save)
+
+  for (let i = 0; i < save.length; i++) {
+    console.log(save[i].id)
+    var dayEvents = save[i].id.split('saveId-').join('');
+    var eventNum = parseInt(dayEvents)
+    console.log(dayEvents)
+    console.log(eventNum)
+  }
 
   save.click(function (e) {
     e.preventDefault;
-    var dayEvents = $('button:.saveId');
-    eventStorage.push(dayEvents);
-    localStorage.setItem('event', JSON.stringify(eventStorage));
+    let saveVal = $('this').children('textarea').val()
+    eventTextArry.push(saveVal);
+    localStorage.setItem('event', JSON.stringify(eventTextArry));
     console.log(localStorage.event)
   })
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
