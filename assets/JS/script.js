c60e9055ff3799585ec3a71d604bc77030b3b250
Mr. Macro <mr.matt.caschetto@gmail.com>
2023-02-28 17:10:48 -0500

    Commit to push finalized code to repo.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,22 +7,10 @@ $(function dayPlanner() {
   var save = $('.saveBtn');
   var hourIndex = $('.dailyPlan')
 
-  //Creates local storage array for saved events
-  let eventStorage = JSON.parse(localStorage.getItem('event'));
-
-  if (eventStorage !== null) {
-    eventTextArry = eventStorage;
-  } else {
-    eventTextArry = new Array(9)
-  };
-  console.log(eventTextArry)
   //Function to add IDs to all textareas
   function addId() {
     for (let i = 0; i < hourIndex.length; i++) {
       hourIndex[i].id = 'hour-' + (i)
-      // console.log(hourIndex, currentTime.$H - 9)
-      // console.log(hourIndex[i].id, hourId)
-      // console.log(hourNum < currentTime.$H - 9, hourNum === currentTime.$H - 9, hourNum > currentTime.$H - 9)
     }
   }
 
@@ -31,7 +19,7 @@ $(function dayPlanner() {
     for (let i = 0; i < hourIndex.length; i++) {
       var hourId = hourIndex[i].id.split('hour-').join('')
       var hourNum = parseInt(hourId);
-      // console.log(hourPlan.id)
+
       if (hourNum < currentTime.$H - 9) {
         // console.log("if")
         hourIndex[i].classList.remove('future');
@@ -50,9 +38,7 @@ $(function dayPlanner() {
 
   addId();
   addRemoveClass();
-  // console.log(hourIndex)
-  // console.log(currentTime.$H - 9)
-  // console.log(eventTextArry)
+
   function addSaveId() {
     for (let i = 0; i < hourIndex.length; i++) {
       save[i].id = 'saveId-' + (i)
@@ -69,19 +55,29 @@ $(function dayPlanner() {
     console.log(eventNum)
   }
 
+  //Function used to save text values to local storage based on id
   save.click(function (e) {
     e.preventDefault;
-    let saveVal = $('this').children('textarea').val()
-    eventTextArry.push(saveVal);
-    localStorage.setItem('event', JSON.stringify(eventTextArry));
-    console.log(localStorage.event)
+    console.log(e.target.id)
+    var dayEvents = e.target.id.split('saveId-').join('');
+    var eventNum = parseInt(dayEvents)
+    console.log(eventNum);
+    console.log($('textarea').val())
+    
+    var key = eventNum
+    var event = $('textarea').val()
+    // eventStorage.push(event);
+    localStorage.setItem(key, JSON.stringify(event));
   })
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  //Code used to add text content to textarea based on id
+  for (let i = 0; i < 9; i++) {
+    let key = i
+    console.log(key)
+    hourIndex[i].textContent = localStorage.getItem(JSON.parse(key))
+  }
+  
+  //Code to display the current date in the header of the page.
   timeHeader.text(currentTime.format('dddd, MMM DD'));
 });
 
